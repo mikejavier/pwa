e2b86825c048942572d3708b43c0ec5b3b9ebfca
Michael Santillán <contato@michaelsantillan.com>
2017-03-04 00:29:25 -0300

    rename caches names

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
-var cacheName = 'weatherPWA-step-6-1';
-var dataCacheName = 'weatherData-v1';
+var cacheName = 'weatherPWA';
+var dataCacheName = 'weatherData';
 var filesToCache = [
   './',
   './index.html',
